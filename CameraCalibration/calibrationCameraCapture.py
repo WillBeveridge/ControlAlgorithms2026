738eab45aa6d66d46619e78cb0c571df58a66acb
Will Beveridge <159801488+FrisbeeGod@users.noreply.github.com>
2026-05-05 09:26:26 -0400

    inputting new set of calibration photos taken May 5th 2026

--- CameraCalibration/calibrationCameraCapture.py
+++ CameraCalibration/calibrationCameraCapture.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:581ed136b8c97ad6d7525ba913d5698f2f005af4acf5b8ead3cca22f3a87f160
-size 724
+oid sha256:3882b69d0b3e796eff0999fce4a1ee41c278a0858fb20d16dbbdc3d9dcfeb53d
+size 725
--- CameraCalibration/findFocus.py
+++ CameraCalibration/findFocus.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8db0107585c1b8d3d19e4144f37242754cbded46a8f60f36e800994fb1515e14
+oid sha256:c217c281cd2dc275d352a98aa66e56d86ed94bc680f2245e95f585c3b2c76614
 size 741
